1e832dbacb51e1977223eac0e56bed1976c6852d
shuma1026 <10schumacher26@gmail.com>
2024-07-11 11:31:08 +0900

    shuma　管理者ログイン修正

--- login_1.php
+++ login_1.php
@@ -2,17 +2,19 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>ログイン</title>
-    <link rel="stylesheet" href="css/login.css">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="css/login_1.css">
 </head>
 <body>
-    <h1>ログイン</h1>
-    <form action="login_2.php" method="post">
-        <label for="admin_address">メールアドレス</label>
-        <input type="text" id="admin_address" name="admin_address" required><br>
-        <label for="admin_password">パスワード</label>
-        <input type="text" id="admin_password" name="admin_password" required><br>
-        <input type="submit" value="ログイン">
-    </form>
+    <div class="login-container">
+        <h1>管理者ログイン</h1>
+        <form action="login_2.php" method="post">
+            <label for="admin_address">メールアドレス</label>
+            <input type="text" id="admin_address" name="admin_address" required><br>
+            <label for="admin_password">パスワード</label>
+            <input type="password" id="admin_password" name="admin_password" required><br>
+            <input type="submit" value="ログイン">
+        </form>
+    </div>
 </body>
 </html>
