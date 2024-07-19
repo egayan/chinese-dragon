40a33c22736a3ece64d3674691b57f6642cb01a7
9hyfll <148170801+9hyfll@users.noreply.github.com>
2024-07-19 11:27:01 +0900

    kiyomatsu

--- Top.php
+++ Top.php
@@ -12,166 +12,97 @@
 <div class="pat">
 <div class="A">
 <?php
-if(isset($_GET['gest'])){
-?>
-    <!-- メニュー -->
-    <table align="center">
-    
-    <tr><td><div align="center"><img src="images/logo.jpg" class="logo"></div></td></tr>
-    <tr><td><div align="center">機能を利用するには<button><a href="login_input.php">ログイン画面へ戻る</a></button></div></td></tr>
-    
-    <?php
-    $pdo = new PDO($connect,USER,PASS);
-    $tr=0;
-    $sql = $pdo->query('select * from thread');
-    
+function displayThreads($pdo) {
+    $sql = $pdo->query('SELECT * FROM thread');
+    $tr = 0;
     echo '<div align="center">スレッド一覧</div>';
-    echo '<tr>';
-    echo '<td>';
-    echo '<div  align="left" >';
-    foreach($sql as $row){
-        
-        echo '<a href="******.php?title=',$row['title'],'">',$row['title'],'</a>';
-        $tr++;
-        
-    }
-    echo '</div>';
-    echo '</td>';
-    echo '</tr>';
+    echo '<div style="display: flex; justify-content: center;"><table>';
     echo '<tr><td><div align="center">';
-    echo '<button><a href="Popularity.php?gest=gest">人気スレッドへ</a></button></div></td></tr>';
-    
-    echo '<tr><td>';
-    echo '<div align="center">';
-    echo '<button><a href="warning.php">使い方・注意</a></button></div></td></tr>';
-    echo '</table>';
+    foreach($sql as $row) {
+        echo '<a href="******.php?title=', $row['title'], '">', $row['title'], '</a>';
+        $tr++;
+        if ($tr == 3) {
+            echo '</div></td></tr><tr><td><div align="center">';
+            $tr = 0;
+        }
+    }
+    echo '</div></td></tr>';
+    echo '</table></div>';
+}
+if (isset($_GET['gest'])) {
+?>
+    <table align="center">
+        <tr><td><div align="center"><img src="images/logo.jpg" class="logo"></div></td></tr>
+        <tr><td><div align="center">機能を利用するには<button><a href="login_input.php">ログイン画面へ戻る</a></button></div></td></tr>
+    </table>
+    <?php
+    $pdo = new PDO($connect, USER, PASS);
+    displayThreads($pdo);
     ?>
-
-<?php
-}else{
-if(isset($_POST['passward']) || isset($_POST['login'])){
-    unset($_SESSION['customer']);
-    $_SESSION['login']=[
-        'id'=>0
-    ];
-if($_POST['password'] != null && $_POST['login'] != null){
-$pdo = new PDO($connect,USER,PASS);
-$sql = $pdo->prepare('select * from client where client_address=?');
-$sql->execute([$_POST['login']]);
-foreach($sql as $row){
-    if($_POST["password"]==$row['password']){
-    $_SESSION['customer']=[
-        'id'=>$row['client_id'],'name'=>$row['name'],
-        'password'=>$row['password'],'address'=>$row['client_address']
-    ];
-    }
-}
-
-if(isset($_SESSION['customer'])){
-    /*echo 'ようこそ、',$_SESSION['customer']['name'],'さん。';*/
-    $_SESSION['login']=[
-        'id'=>1
-    ];
-}else{
-    echo '<div class="logerror">';
-    echo 'ログイン名またはパスワードが違います。';
-    echo '<br>';
-    echo '<a href="login_input.php" class="button">ログインへ</a>';
-    echo '</div>';
-}
-}else{
-    echo '<div class="logerror">';
-    echo 'ログイン名またはパスワードを入力してください。';
-    echo '<br>';
-    echo '<a href="login_input.php" class="button">ログインへ</a>';
-    echo '</div>';
-}
-
-}
-if($_SESSION['login']['id']==1){
-
-$freeze_check = new PDO($connect,USER,PASS);
-$freeze_check = $pdo->prepare('select * from client where client_address=?');
-$freeze_check ->execute([$_POST['login']]);
-foreach($freeze_check as $row){
-$check=$row['freeze'];
-}
-if($check == 1){
-?>
-
-<table align="center">
-  <tr><td><div align="center"><img src="images/logo.jpg" class="logo">
-  <form action="login_input.php" method="post">
-        <input type="submit" value="ログアウト" size="35" >
-</form>
-  
-  <tr><td><div align="center">
-  <form action="Top_kensakukekka.php" method="post">
-    <input type="text" placeholder="検索" name="kensaku" size="70" ><input type="submit" value="検索" size="35" >
-  </form>
-  </div></td></tr>
-  <tr><td><div class="line"></div></td></tr>
-  
-<?php
-$pdo = new PDO($connect, USER, PASS);
-$tr = 0;
-$sql = $pdo->query('SELECT * FROM thread');
-?>
-
-<!-- テーブルを中央寄せ -->
-<div style="display: flex; justify-content: center;">
-
-    
-        <tr>
-            <td><div align="center">スレッド一覧</div></td>
-        </tr>
-        <tr>
-            <td>
-                <div align="center">
-                    <?php
-                    foreach ($sql as $row) {
-                        echo '<a href="******.php?title=', $row['title'], '">', $row['title'], '</a>';
-                        $tr++;
-                        if ($tr == 3) {
-                            echo '</div>';
-                            echo '</td>';
-                            echo '</tr>';
-                            $tr = 0;
-                            echo '<tr>';
-                            echo '<td>';
-                            echo '<div align="center">';
-                        }
-                    }
-                    ?>
-                </div>
-            </td>
-        </tr>
-
-    
-        <tr><td><div class="line"></div></td></tr>
-    <tr><td>
     <div align="center">
-    <button><a href="thread-write.php" style="color: #fff;">新規スレッド書き込み画面へ</a></button>
-    <button><a href="Popularity.php" style="color: #fff;">人気スレッドへ</a></button>
-    <button><a href="chat.php"style="color: #fff;">個人チャット</a></button>
-    <button><a href="mypage.php" style="color: #fff;">マイページ</a></button>
-    <button><a href="inquiry.php" style="color: #fff;">お問い合わせ</a></button>
-    <button><a href="warning.php" style="color: #fff;">使い方・注意</a></button></div></td></tr></div>
-    
-</table>
-</div>
-
-
-<?php 
-    }else{
-        echo '<h1>このアカウントは凍結されています</h1>';
+        <button><a href="Popularity.php?gest=gest">人気スレッドへ</a></button>
+        <button><a href="warning.php">使い方・注意</a></button>
+        <button><a href="genre.php" style="color: #fff;">スレッド一覧画面へ</a></button>
+    </div>
+<?php
+} else {
+    if (isset($_POST['password']) || isset($_POST['login'])) {
+        unset($_SESSION['customer']);
+        $_SESSION['login'] = ['id' => 0];
+        if ($_POST['password'] != null && $_POST['login'] != null) {
+            $pdo = new PDO($connect, USER, PASS);
+            $sql = $pdo->prepare('SELECT * FROM client WHERE client_address=?');
+            $sql->execute([$_POST['login']]);
+            foreach ($sql as $row) {
+                if ($_POST["password"] == $row['password']) {
+                    $_SESSION['customer'] = [
+                        'id' => $row['client_id'], 'name' => $row['name'],
+                        'password' => $row['password'], 'address' => $row['client_address']
+                    ];
+                }
+            }
+            if (isset($_SESSION['customer'])) {
+                $_SESSION['login'] = ['id' => 1];
+            } else {
+                echo '<div class="logerror">ログイン名またはパスワードが違います。<br><a href="login_input.php" class="button">ログインへ</a></div>';
+            }
+        } else {
+            echo '<div class="logerror">ログイン名またはパスワードを入力してください。<br><a href="login_input.php" class="button">ログインへ</a></div>';
+        }
     }
-}
+    if ($_SESSION['login']['id'] == 1) {
+        $freeze_check = $pdo->prepare('SELECT * FROM client WHERE client_address=?');
+        $freeze_check->execute([$_POST['login']]);
+        foreach ($freeze_check as $row) {
+            $check = $row['freeze'];
+        }
+        if ($check == 1) {
 ?>
-<?php 
+            <table align="center">
+                <tr><td><div align="center"><img src="images/logo.jpg" class="logo"><form action="login_input.php" method="post"><input type="submit" value="ログアウト" size="35"></form></div></td></tr>
+                <tr><td><div align="center"><form action="Top_kensakukekka.php" method="post"><input type="text" placeholder="検索" name="kensaku" size="70"><input type="submit" value="検索" size="35"></form></div></td></tr>
+                <tr><td><div class="line"></div></td></tr>
+            </table>
+            <?php
+            $pdo = new PDO($connect, USER, PASS);
+            displayThreads($pdo);
+            ?>
+            <div align="center">
+                <button><a href="thread-write.php" style="color: #fff;">新規スレッド書き込み画面へ</a></button>
+                <button><a href="genre.php" style="color: #fff;">スレッド一覧画面へ</a></button>
+                <button><a href="Popularity.php" style="color: #fff;">人気スレッドへ</a></button>
+                <button><a href="chat.php"style="color: #fff;">個人チャット</a></button>
+                <button><a href="mypage.php" style="color: #fff;">マイページ</a></button>
+                <button><a href="inquiry.php" style="color: #fff;">お問い合わせ</a></button>
+                <button><a href="warning.php" style="color: #fff;">使い方・注意</a></button>
+            </div>
+<?php
+        } else {
+            echo '<h1>このアカウントは凍結されています</h1>';
+        }
+    }
 }
 ?>
 <?php require 'footer.php'; ?>
 </div>
-                </div>
-
+</div>
--- Top_kensakukekka.php
+++ Top_kensakukekka.php
@@ -97,6 +97,7 @@ echo '</div>';
 <div class="menu">
     <tr><td><div align="center">
     <button><a href="thread-write.php" style="color: #fff;">新規スレッド書き込み画面へ</a></button>
+    <button><a href="genre.php" style="color: #fff;">スレッド一覧画面へ</a></button>
     <button><a href="Popularity.php" style="color: #fff;">人気スレッドへ</a></button>
     <button><a href="chat.php" style="color: #fff;">個人チャット</a></button>
     <button><a href="mypage.php" style="color: #fff;">マイページ</a></button>
